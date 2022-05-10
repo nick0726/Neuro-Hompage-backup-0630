734ee894f2e95dc9e945017f058bb272c804dbf6
Yujin0827 <yjjeon980827@gmail.com>
2022-05-10 16:30:23 +0900

    add file

--- src/App.js
+++ src/App.js
@@ -4,22 +4,8 @@ import Header from "./components/Header"
 
 function App() {
   return (
-    <div className="App">
-      <Header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </Header>
-    </div>
-  );
+    <></>
+  )
 }
 
 export default App;
