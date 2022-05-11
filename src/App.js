25fef4b2ffd99b90424ac6e1a374d838dce2c0f9
nick0726 <nick0726@naver.com>
2022-05-11 13:37:22 +0900

    Construct frame for MainPage, Header, Footer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
-
 import Header from "./components/Header";
+import Main from "./pages/main/main";
+import Footer from "./components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 const Footer = () => {
-    return (
-        <footer>
-            <h2>This is Footer</h2>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <h2>This is Footer</h2>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
