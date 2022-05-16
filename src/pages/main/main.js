48dfb7d6357ebcb08010f392137cb3cd11095693
nick0726 <nick0726@naver.com>
2022-05-16 17:42:55 +0900

    Update MainPage and Header with CSS Update

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -7,12 +7,30 @@ function Main() {
   return (
     <>
       <Header />
-      <div className='main'>
-        <div className='item'>Page1</div>
-        <div className='item'>Page2</div>
-        <div className='item'>Page3</div>
-        <div className='item'>Page4</div>
-        <div className='item'>Page5</div>
+      <div className='main_container'>
+        <div className='main_container_content'>
+          <div className='main_container_navigator'>
+            어딧니
+            <div className='mainpage_page1_title'>
+              <h1>NeuroEars</h1>
+              <h4>평형기능</h4>
+              <h4>소프트웨어 개발</h4>
+              <h4>전문기업</h4>
+              <div className='mainpage_page1_subtitle'>
+                <h5>Software as Medical Device for</h5>
+                <h5>Vestibular function tests and rehabilitation</h5>
+              </div>
+            </div>
+            <div className='mainpage_page1_btn'>
+              <button>문의하기</button>
+            </div>
+          </div>
+        </div>
+
+        <div className='main'>Page2</div>
+        <div className='main'>Page3</div>
+        <div className='main'>Page4</div>
+        <div className='main'>Page5</div>
       </div>
       <Footer />
     </>
