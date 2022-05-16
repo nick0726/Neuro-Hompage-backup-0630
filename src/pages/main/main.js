f1a957734881e5ff2184945a42ee28d7a585f418
nick0726 <nick0726@naver.com>
2022-05-17 08:58:13 +0900

    Update MainPage

--- src/pages/main/main.js
+++ src/pages/main/main.js
@@ -2,34 +2,51 @@ import React from "react";
 import "./Main.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
+import Fullpage, {
+  FullPageSections,
+  FullpageSection,
+  FullpageNavigation,
+} from "@ap.cx/react-fullpage";
 
 function Main() {
   return (
     <>
       <Header />
-      <div className='main_container'>
-        <div className='main_container_content'>
-          <div className='mainpage_page1_title'>
-            <h1>NeuroEars</h1>
-            <h4>평형기능</h4>
-            <h4>소프트웨어 개발</h4>
-            <h4>전문기업</h4>
-            <div className='mainpage_page1_subtitle'>
-              <h5>Software as Medical Device for</h5>
-              <h5>Vestibular function tests and rehabilitation</h5>
-              <div className='mainpage_page1_btn'>
-                <button>문의하기</button>
+      <Fullpage>
+        <FullpageNavigation />
+        <FullPageSections>
+          <FullpageSection>
+            <div className='main_container'>
+              <div className='main_container_content'>
+                <div className='mainpage_page1_title'>
+                  <h1>NeuroEars</h1>
+                  <h4>평형기능</h4>
+                  <h4>소프트웨어 개발</h4>
+                  <h4>전문기업</h4>
+                  <div className='mainpage_page1_subtitle'>
+                    <h5>Software as Medical Device for</h5>
+                    <h5>Vestibular function tests and rehabilitation</h5>
+                    <div className='mainpage_page1_btn'>
+                      <button>문의하기</button>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='main'>페이지2</div>
+                <div className='main'>페이지3</div>
+                <div className='main'>페이지4</div>
+                <div className='main'>페이지5</div>
               </div>
             </div>
-          </div>
-
-          <div className='main'>Page2</div>
-          <div className='main'>Page3</div>
-          <div className='main'>Page4</div>
-          <div className='main'>Page5</div>
-        </div>
-        <div className='main_container_navigator'>어딧니</div>
-      </div>
+          </FullpageSection>
+          <FullpageSection>
+            <h1>Screen 2</h1>
+          </FullpageSection>
+          <FullpageSection>
+            <h1>Screen 3</h1>
+          </FullpageSection>
+        </FullPageSections>
+      </Fullpage>
       <Footer />
     </>
   );
