e1e20ce7eb34b9d7e3a70161be8ee06af58877a0
nick0726 <nick0726@naver.com>
2022-05-31 10:58:45 +0900

    Update index.js, found header floating bug due to Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import App from "./App";
 import "./index.css";
-import "bootstrap/dist/css/bootstrap.min.css"
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
 import { createRoot } from "react-dom/client";
 
